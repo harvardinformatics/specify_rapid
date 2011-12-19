b2a2ace0289437d7958d67d5b571124bb8b5d3b6
mole <mole@2410e9f4-4f82-4e4f-a33e-bb13db1cb25e>
2011-12-19 18:33:12 +0000

    ISSUE: BugID: 558 PURPOSE: Adding additional needed character ' for verbatim date. DESCRIPTION: Extending the scope of several of the filter.

--- htdocs/ajax_handler.php
+++ htdocs/ajax_handler.php
@@ -66,7 +66,7 @@ if ($connection && $authenticated) {
         	@$collectors= substr(preg_replace('/[^0-9]/','',$_GET['collectors']),0,huh_agentvariant::NAME_SIZE);
         	@$etal= substr(preg_replace('/[^A-Za-z& \.0-9]/','',$_GET['etal']),0,huh_collector::ETAL_SIZE);
         	@$fieldnumber= substr(preg_replace('/[^A-Za-z\- \.0-9]/','',$_GET['fieldnumber']),0,huh_collectingevent::STATIONFIELDNUMBER_SIZE);
-        	@$verbatimdate= substr(preg_replace('/[^A-Za-z\- \/\.\,0-9\[\]]/','',$_GET['verbatimdate']),0,huh_collectingevent::VERBATIMDATE_SIZE);
+        	@$verbatimdate= substr(preg_replace('/[^A-Za-z\- \/\.\,0-9\[\]\']/','',$_GET['verbatimdate']),0,huh_collectingevent::VERBATIMDATE_SIZE);
         	@$datecollected= substr(preg_replace('/[^\-\/0-9]/','',$_GET['datecollected']),0,40);  // allow larger than date to parse ISO date range
         	@$herbariumacronym= substr(preg_replace('/[^A-Z]/','',$_GET['herbariumacronym']),0,huh_fragment::TEXT1_SIZE);
         	@$barcode= substr(preg_replace('/[^0-9]/','',$_GET['barcode']),0,huh_fragment::IDENTIFIER_SIZE);
