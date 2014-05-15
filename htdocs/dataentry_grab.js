dcb1ca0ad08ce1d6da07568cd74a1258bbc8690c
chuck <chuck@2410e9f4-4f82-4e4f-a33e-bb13db1cb25e>
2014-05-15 14:21:02 +0000

    Checkpoint NEVP.

--- htdocs/dataentry_grab.js
+++ htdocs/dataentry_grab.js
@@ -52,46 +52,56 @@ function on_grab_nevp_click() {
 	// TODO: Make this hit NEVP: right now it still hits the exsic server.
 	fp_data_entry_plugin({
 		iframe_parent_id: "fp-data-entry-plugin-goes-here",
-		url: "http://localhost:8080", // Change this to point to the real server.
+		url: "http://fp3.acis.ufl.edu:8888", // Change this to point to the real server.
 		css_urls: [
 			"css/data-entry.css", // default css is provided on server.
 		],
 		js_urls: [
-			"http://localhost/rapid/dataentry_extra.js" 
+			// TODO: Same controlled vocab as for lichens?
+			// "http://localhost/rapid/dataentry_extra.js" 
 		],
 	
 		// optional:
 		width: window.innerWidth * 1/3, // TODO: better logic for this.
 		//height: 800,
 		
-		// To hide input fields, find and remove them in your extra JS.
-		outputs_to_hide: ["exsiccateTitle","exsiccateNumber","countryCode",
-		                  "kingdom", "phylum", "class", "order","scientificNameAuthorship"],
-
+		// Which fields will be used to query the widget?
+		// None of these are required, but at least one should be present.
+		// Your field names should be unique. 
 		q_names: {
-			exsiccate_number: "exsiccatinumber"
+			collector: "collector",
+			collectionNumber: "collectionNumber"
 		},
-		q_ids: {
-			exsiccate_title: "exsiccati"
+		
+		// As changes are made in the widget, which fields in the parent are changed?
+		// Typically, these are input name attributes, but if you need something more flexible,
+		// a function can be given in selector_function.
+		selectors: {
+			"genus":"genus",
+			"specificEpithet":"specificEpithet",
+			"infraspecificRank":"infraspecificRank",
+			"infraspecificEpithet":"infraspecificEpithet",
+			"scientificNameAuthorship":"scientificNameAuthorship",
+			"identificationQualifier":"identificationQualifier",
+			"collector":"collector",
+			"collectionNumber":"collectionNumber",
+			"verbatimCollectionDate":"verbatimCollectionDate"
 		},
-		a_names: {
-			verbatimLocality: "specificlocality",
-			decimalLatitude: "verbatimlat",
-			decimalLongitude: "verbatimlong",
-			eventDate: "datecollected",
-			habitat: "habitat",
-			recordNumber: "fieldnumber",
-			minimumElevationInMeters: "minelevation",
-			maximumElevationInMeters: "maxelevation",
+		selector_function: function(name) {
+			var els = document.getElementsByName(name);
+			if (els.length > 1) {
+				throw Error('Name "'+name+'" is not unique in this document.');
+			};
+			return els[0];
+		},
 
-			recordedBy_id: "collectors",
-			scientificName_id: "filedundername",
-			typeStatus_id: "typestatus"
-		},
-		a_ids: {
-			recordedBy: "collectors",
-			scientificName: "filedundername",
-			typeStatus: "typestatus"
-		}
+		// Whether confirmation is explicitly required, or if data can automatically
+		// be copied over into the host interface.
+		require_confirmation: false,
+
+		// Arbitrary JavaScript to be run against the target page.
+		// This can be useful when the target application needs to be
+		// prompted to create all the form fields we want to fill in.
+		pre_js: "" 
 	});
 }
