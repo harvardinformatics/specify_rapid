520757cb91f39d9bbe5df1b6baf7b0e438345e9e
mole <mole@2410e9f4-4f82-4e4f-a33e-bb13db1cb25e>
2014-08-08 16:16:18 +0000

    ISSUE: PURPOSE: Support old php for FP-DataEntry and rapid on watson. DESCRIPTION: Adding a json_encode function in case php version is too old to have one.

--- htdocs/ajax_handler_jsonp.php
+++ htdocs/ajax_handler_jsonp.php
@@ -25,7 +25,7 @@ $sql = array(
 // Validate
 //
 
-include_once('connection_library_ro.php');
+include_once('connection_library.php');
 if (!function_exists('specify_connect')) {
    die('specify_connect not defined.');
 }
@@ -75,10 +75,85 @@ if (isset($best) && count($matches)>1) {
 // Send it out
 //
 
+if (!defined("json_encode")) { 
+
+/**
+ * Converts an associative array of arbitrary depth and dimension into JSON representation.
+ *
+ * NOTE: If you pass in a mixed associative and vector array, it will prefix each numerical
+ * key with "key_". For example array("foo", "bar" => "baz") will be translated into
+ * {"key_0": "foo", "bar": "baz"} but array("foo", "bar") would be translated into [ "foo", "bar" ].
+ *
+ * @param $array The array to convert.
+ * @return mixed The resulting JSON string, or false if the argument was not an array.
+ * @author Andy Rusterholz
+ */
+function json_encode( $array ){
+
+    if( !is_array( $array ) ){
+        return false;
+    }
+
+    $associative = count( array_diff( array_keys($array), array_keys( array_keys( $array )) ));
+    if( $associative ){
+
+
+        $construct = array();
+        foreach( $array as $key => $value ){
+
+            // We first copy each key/value pair into a staging array,
+            // formatting each key and value properly as we go.
+
+            // Format the key:
+            if( is_numeric($key) ){
+                $key = "key_$key";
+            }
+            $key = '"'.addslashes($key).'"';
+
+            // Format the value:
+            if( is_array( $value )){
+                $value = json_encode( $value );
+            } else if( !is_numeric( $value ) || is_string( $value ) ){
+                $value = '"'.addslashes($value).'"';
+            }
+
+            // Add to staging array:
+            $construct[] = "$key: $value";
+        }
+
+        // Then we collapse the staging array into the JSON form:
+        $result = "{ " . implode( ", ", $construct ) . " }";
+
+    } else { // If the array is a vector (not associative):
+
+        $construct = array();
+        foreach( $array as $value ){
+
+            // Format the value:
+            if( is_array( $value )){
+                $value = json_encode( $value );
+            } else if( !is_numeric( $value ) || is_string( $value ) ){
+                $value = '"'.addslashes($value).'"';
+            }
+
+            // Add to staging array:
+            $construct[] = $value;
+        }
+
+        // Then we collapse the staging array into the JSON form:
+        $result = "[ " . implode( ", ", $construct ) . " ]";
+    }
+
+    return $result;
+} 
+
+}
+
 $json = json_encode(array(
   'name' => $_GET['name'],
   'list' => $matches
 ));
+
 
 echo $_GET['callback'] . '(' . $json . ');';
 
