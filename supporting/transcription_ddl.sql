0aaa71b083087ef6f9ee5fa476bc3b99540e6ada
Paul J. Morris <mole@morris.net>
2018-08-11 21:05:48 -0400

    ISSUE: #1 PURPOSE: Document added tables to support transcription. DESCRIPTION: File with ddl documenting three added table for support of transcription from file system and logging of transcription events (prefixed TR_ for transcription).

---
+++ supporting/transcription_ddl.sql
@@ -0,0 +1,29 @@
+create table TR_BATCH (
+   -- filesystem based batches of images for transcription
+   tr_batch_id bigint not null auto_increment primary key,  -- surrogate numeric primary key
+   path varchar(2000) not null,  -- IMAGE_LOCAL_FILE.path to batch
+   image_batch_id bigint, -- foreign key to IMAGE_BATCH
+   completed_date date default null -- set date when batch is completed
+);
+
+create table TR_USER_BATCH ( 
+   -- pending, in progress, and completed batches for users.
+   tr_user_batch_id bigint not null auto_increment primary key, -- surrogate numeric primary key
+   tr_batch_id bigint not null, 
+   username varchar(64) not null,-- specifyuser.name, huh_webuser.username
+   position int not null default 0, -- last file transcribed in batch
+
+   KEY IDXUSERBATCHBATCHID (tr_batch_id),
+   CONSTRAINT FKUSERBATCHBATCHID FOREIGN KEY (tr_batch_id) REFERENCES TR_BATCH (tr_batch_id),
+   CONSTRAINT FKUSERBATCHUSERNAME FOREIGN KEY (username) REFERENCES specifyuser (name)
+
+);
+
+create table TR_ACTION_LOG ( 
+   -- track transcription activity
+   tr_action_log_id bigint not null auto_increment primary key, -- surrogate numeric primary key
+   username varchar(64) not null,-- specifyuser.name, huh_webuser.username
+   action_time timestamp default CURRENT_TIMESTAMP not null,  -- timestamp for the action
+   action varchar(50) not null, -- specific action carried out
+   details text -- remarks about the action
+);
