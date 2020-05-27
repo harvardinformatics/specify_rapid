05c6d8a553cc3be710859fe4881888a9c0ce7a78
Jonathan Kennedy <jonathan_kennedy@harvard.edu>
2020-05-27 15:22:15 -0400

    Upgraded jquery

--- htdocs/transcribe_lib.php
+++ htdocs/transcribe_lib.php
@@ -512,10 +512,11 @@ class TPage extends Page {
 
    public function getJQueryPageHead() {
      $returnvalue = "
-       <script src='/jquery/jquery-3.2.1.min.js'></script>
+       <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js'></script>
        <script src='/jquery/zoom-master/jquery.zoom.js'></script>
-       <script src='/jquery/jquery-ui-1.12.1.custom/jquery-ui.min.js'></script>
-       <link rel='stylesheet' type='text/css' href='/jquery/jquery-ui-1.12.1.custom/jquery-ui.theme.min.css' />
+       <link rel='stylesheet' href='https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css'>
+       <script src='https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js'></script>
+
        <script>
          $( ':submit' ).button({ classes: { 'ui-button': 'highlight' } });
        </script>
