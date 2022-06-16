b81da9c6757d33e3a302bd1d4cf4563f37c626b2
jakennedy <jonathan_kennedy@harvard.edu>
2022-06-16 12:02:49 -0400

    Added country to collector drop down

--- htdocs/class_lib.php
+++ htdocs/class_lib.php
@@ -1052,7 +1052,19 @@ class huh_collector_custom extends huh_collector {
    public function keySelectCollectorAgentIDJSON($term) {
       global $connection;
       $returnvalue = '[';
-      $preparemysql = " SELECT agentvariant.agentid, concat(name,' [',case agenttype when 0 then 'Organization' when 1 then 'Individual' when 2 then 'Other' when 3 then 'Team' end, ' ',coalesce(year(agent.dateofbirth),'?'),'-',coalesce(year(agent.dateofdeath),'?'),']') as label, name as value FROM agentvariant left join agent on agentvariant.agentid = agent.agentid where name like ? and vartype = 4 order by name ASC ";
+      //$preparemysql = " SELECT agentvariant.agentid, concat(name,' [',case agenttype when 0 then 'Organization' when 1 then 'Individual' when 2 then 'Other' when 3 then 'Team' end, ' ',coalesce(year(agent.dateofbirth),'?'),'-',coalesce(year(agent.dateofdeath),'?'),']') as label, name as value FROM agentvariant left join agent on agentvariant.agentid = agent.agentid where name like ? and vartype = 4 order by name ASC ";
+      $preparemysql = """
+        SELECT agentvariant.agentid, concat(agentvariant.name,' [',case agenttype when 0 then 'Organization' when 1 then 'Individual' when 2 then 'Other' when 3 then 'Team' end, ' ',coalesce(year(agent.dateofbirth),'?'),'-',coalesce(year(agent.dateofdeath),'?'),']',' (',coalesce(GROUP_CONCAT(geography.FullName SEPARATOR ', ')),')') as label, agentvariant.name as value
+        FROM agentvariant
+        left join agent on agentvariant.agentid = agent.agentid
+        left join agentgeography on agentvariant.agentid = agentgeography.agentid
+        left join geography on geography.geographyid = agentgeography.geographyid
+        where agentvariant.vartype = 4
+          and agentgeography.role = 'Collector'
+          and name like ?
+        group by agentvariant.agentid
+        order by agentvariant.name ASC
+        """
       $comma = '';
       if ($stmt = $connection->prepare($preparemysql)) {
          $stmt->bind_param('s',$term);
