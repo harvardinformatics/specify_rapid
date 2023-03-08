d493e7e4e1518836b9b2a18bc27ac5ab4e12ed3e
jakennedy <jonathan_kennedy@harvard.edu>
2023-03-08 15:46:24 -0500

    Update transcribe.php

--- htdocs/transcribe.php
+++ htdocs/transcribe.php
@@ -1561,7 +1561,7 @@ function selectTaxon($field,$label,$value,$valueid,$required='false',$carryforwa
             $( "#'.$field.'" ).autocomplete({
                minLength: 4,
                delay: 500,
-               searchAttr='label',
+               searchAttr="label",
                source: function( request, response ) {
                   $.ajax( {
                     url: "ajax_handler.php",
