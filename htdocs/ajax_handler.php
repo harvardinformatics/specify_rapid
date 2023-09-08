52bf2a03765cf98de28dd1ea72a02c5591ea2634
jakennedy <jonathan_kennedy@harvard.edu>
2023-09-08 00:05:17 -0400

    Update ajax_handler.php

--- htdocs/ajax_handler.php
+++ htdocs/ajax_handler.php
@@ -338,7 +338,7 @@ if ($connection && $authenticated) {
          } else {
             $values = "[]";
          }
-         header("Content-type text/json-comment-filtered");
+         header("Content-Type application/json");
          if ($ok) {
             $response = '';
             //echo '{ "identifier":"value", "label":"name",';
